cbf9e3b163b7d71aa289d75b4ae350bc607d0298
Jason Gonzales <jason.gonzales23@gmail.com>
2014-02-09 13:02:27 -0800

    finish step 0305

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+//beerlist birdwatcher list javascript
+
+var hideShowFunction = function (evt) {
+  var eventTarget = $(evt.target);
+  var data = eventTarget.data();
+  $('.view').removeClass('active');
+  $(data.target).addClass('active')
+}
+
+$('.view-switcher').on('click', function(evt){
+  hideShowFunction(evt);
+});
