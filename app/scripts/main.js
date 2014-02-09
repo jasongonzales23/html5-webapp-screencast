f69953fc3b23b66ef870474e56672b585aa77af5
Jason Gonzales <jason.gonzales23@gmail.com>
2014-02-09 14:21:05 -0800

    finish 0403

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,7 +13,31 @@ $('.view-switcher').on('click', function(evt){
 
 //localStorage
 if (Modernizr.localstorage) {
-  console.log('it works!');
+  var beers = [
+    {
+    "id": "1",
+    "title": "Pliny the Younger",
+    "description" : "Good!",
+    "image" : null,
+    "location" : { "latitude" : 42, "longitude" : 42 }
+    },
+    {
+    "id": "2",
+    "title": "Racer 5",
+    "description" : "Excellent",
+    "image" : null,
+    "location" : [42, 42]
+    },
+    {
+    "id": "3",
+    "title": "Pliny the Elder",
+    "description" : "Good!",
+    "image" : null,
+    "location" : [42, 42]
+    }
+  ];
+
+  localStorage.beers = JSON.stringify(beers);
 }
 else {
   console.log('it does not work');
