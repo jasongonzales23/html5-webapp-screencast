5a8a14b6279984d0f8d09647922c4fc18533e337
Jason Gonzales <jason.gonzales23@gmail.com>
2014-02-09 13:59:55 -0800

    finish 0401

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,8 +5,16 @@ var hideShowFunction = function (evt) {
   var data = eventTarget.data();
   $('.view').removeClass('active');
   $(data.target).addClass('active')
-}
+};
 
 $('.view-switcher').on('click', function(evt){
   hideShowFunction(evt);
 });
+
+//localStorage
+if (Modernizr.localstorage) {
+  console.log('it works!');
+}
+else {
+  console.log('it does not work');
+}
